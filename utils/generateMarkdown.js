e6b80d2fe736e8138cea2862eabe6c9d54ada353
flcn <jfalconpriv@gmail.com>
2024-06-25 11:45:23 -0400

    updated generateMarkdown helpers

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,38 @@
+import { readFileSync } from 'fs';
+const licenses = JSON.parse(readFileSync('utils/common-licenses.json', 'utf8'));
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === 'MIT') {
-    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
-  } else if (license === 'Apache 2.0') {
-    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-  } else if (license === 'GPL 3.0') {
-    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
-  } else {
+  if (licenses[license]) {
+    return licenses[license].badge;
+  }
+  else {
     return '';
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (licenses[license]) {
+    return licenses[license].url;
+  }
+  else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (licenses[license]) {
+    return `This project is licensed under the ${license} license.`;
+  }
+  else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
